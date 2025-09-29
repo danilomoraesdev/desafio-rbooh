6b72f3fab53ec7625add348dbd50570a77e9111a
Danilo Moraes <danilo.ms@hotmail.com>
2025-09-29 17:21:54 -0300

    feat(backend): adiciona suporte ao Prisma e configurações do Swagger para documentação da API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const doc = new DocumentBuilder()
+    .setTitle('API Pontos de Mídia')
+    .setDescription('API REST para gerenciamento de pontos de mídia')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, doc);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
